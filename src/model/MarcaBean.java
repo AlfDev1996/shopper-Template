f9bcba3a6296da9e02a891d04f8f8fbc3941996e
the_l <the_l@DESKTOP-4P4DHMS>
2018-06-25 23:22:41 +0200

    Commit MarcaBean

--- src/model/MarcaBean.java
+++ src/model/MarcaBean.java
@@ -2,29 +2,27 @@ package model;
 
 public class MarcaBean {
 
-	
-	
+	private int id_marca;
+	private String descrizione;
 	
 	public MarcaBean() {
-		this.id_marca=0;
 		this.descrizione="";
 	}
 	
 	
 	public MarcaBean(int id_marca, String descrizione) {
-		
 		this.id_marca = id_marca;
 		this.descrizione = descrizione;
 	}
 	
 	
 	
-	public int getId_marca() {
+	public int getIdMarca() {
 		return id_marca;
 	}
 
 
-	public void setId_marca(int id_marca) {
+	public void setIdMarca(int id_marca) {
 		this.id_marca = id_marca;
 	}
 
@@ -47,11 +45,5 @@ public class MarcaBean {
 		return "marca : [id_marca=" + id_marca + ", descrizione=" + descrizione + "]";
 	}
 
-
-
-
-
-	private int id_marca;
-	private String descrizione;
 	
 }
