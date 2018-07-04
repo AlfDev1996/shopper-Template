5f6d1efbfd8c8816e5e541bac10789a04f8cefec
the_l <the_l@DESKTOP-4P4DHMS>
2018-07-04 21:33:08 +0200

    .

--- src/controller/ModificaAccount.java
+++ src/controller/ModificaAccount.java
@@ -19,6 +19,8 @@ public class ModificaAccount extends HttpServlet {
      */
     public ModificaAccount() {
         super();
+        
+        
         // TODO Auto-generated constructor stub
     }
 
